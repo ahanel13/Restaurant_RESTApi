df7e60fd33717d820a3993909b97452647fb31c1
Anthony Hanel <ahanel13@gmail.com>
2020-03-26 16:21:59 -0500

    adding headers to avoid any CORS error that could occur in browers

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 //const morgan = require('morgan');
 
+//adding headers to avoid CORS errors
+app.use((req, res, next) => {
+    //giving access to any origin becuase it is a public api
+    res.header('Access-Control-Allow-Origin', '*');
+    //type of headers that can be sent with a request
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE')
+        return res.status(200).json({});
+    }
+});
+
 //importing the routes
 const compsRoutes = require('./api/routes/comps');
 const employeesRoutes = require('./api/routes/employees');
