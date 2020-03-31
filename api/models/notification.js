d6f84a857f829acb7b7c25de545300cb1a679b79
Anthony Hanel <ahanel13@gmail.com>
2020-03-30 23:35:48 -0500

    add notification type to model

--- api/models/notification.js
+++ api/models/notification.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const notifcationSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     employee_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true }, // FIXME: 
-    menuItem_id: { type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem', required: true }
+    notificationType: { type:String, required: true}
 });
 
 module.exports = mongoose.model('Notification', notifcationSchema);
