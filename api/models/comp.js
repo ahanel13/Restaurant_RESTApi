e2374210d7c9150dd70d1b7f1c90097ddff2d5bc
Anthony Hanel <ahanel13@gmail.com>
2020-03-30 23:34:54 -0500

    added timestamp to comps model

--- api/models/comp.js
+++ api/models/comp.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 //Creating a item schema
 const compSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    employee_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true }, // FIXME: 
+    employee_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true }, 
     menuItem_id: { type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem', required: true }
-});
+},
+    {timestamp: true}    
+);
 
 module.exports = mongoose.model('Comp', compSchema);
