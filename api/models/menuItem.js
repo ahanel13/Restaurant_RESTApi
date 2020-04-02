af47123b02c2c890259332a4193445ca69c92718
Anthony Hanel <ahanel13@gmail.com>
2020-04-02 14:06:04 -0500

    updated spelling

--- api/models/menuItem.js
+++ api/models/menuItem.js
@@ -8,7 +8,7 @@ const menuItemSchema = mongoose.Schema({
     ],
     name: { type: String, required: true},
     picture: String,
-    desctription: { type: String, required: true},
+    description: { type: String, required: true},
     price: { type: Number, required: true},
     nutrition: { type: String, required: true},
     item_type: { type: String, required: true},
